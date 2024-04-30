a1eb7b7ae7acc6a98c53d2d3a6558691a52a7ca5
Alain Mena Galindo <alain@Alains-MacBook-Pro.local>
2024-04-29 23:07:33 -0400

    [1] Up.

---
+++ README.md
@@ -0,0 +1,97 @@
+
+# Get Started
+
+## Install
+
+```
+npm install express-route-lister
+```
+
+## Import
+
+```
+const expressRouteLister = require('express-route-lister');
+```
+
+## Log Routes on Init.
+
+```
+app.listen(3001, '127.0.0.1', (e) =>
+{
+	expressRouteLister.log(app); // log all routes on app. start
+});
+```
+
+🚥 2024-04-30T02:49:45.136Z [ null, '*' ] [ null, '/' ] (/routes/index.js)
+🚥 2024-04-30T02:49:45.137Z [ null, '*' ] [ null, '/' ] (/routes/index.js)
+🚥 2024-04-30T02:49:45.137Z [ null, '*' ] [ null, '/' ] (/routes/index.js)
+🚥 2024-04-30T02:49:45.137Z [ null, '*' ] [ null, '/' ] (/routes/index.js)
+🚥 2024-04-30T02:49:45.137Z [ null, '*' ] [ null, '/' ] (/routes/index.js)
+🚥 2024-04-30T02:49:45.137Z [ null, '*' ] [ null, '/' ] (/routes/index.js)
+🚥 2024-04-30T02:49:45.137Z [ null, 'GET' ] [ null, '/' ] (/routes/index.js)
+🚥 2024-04-30T02:49:45.137Z [ null, '*' ] [ null, '/_:id' ] (/routes/api/bin.js)
+🚥 2024-04-30T02:49:45.137Z [ null, '*' ] [ null, '/_:id/*' ] (/routes/api/bin.js)
+🚥 2024-04-30T02:49:45.137Z [ null, '*' ] [ null, '/_*' ] (/routes/api/bin.js)
+🚥 2024-04-30T02:49:45.138Z [ null, '*' ] [ null, '/api/bin/_:id' ] (/routes/api/bin.js)
+🚥 2024-04-30T02:49:45.138Z [ null, '*' ] [ null, '/api/bin/_:id/*' ] (/routes/api/bin.js)
+🚥 2024-04-30T02:49:45.138Z [ null, '*' ] [ null, '/api/bin/_*' ] (/routes/api/bin.js)
+🚥 2024-04-30T02:49:45.138Z [ null, 'POST' ] [ null, '/api/obs/actions/restart' ] (/routes/api/obs-sub.js)
+🚥 2024-04-30T02:49:45.138Z [ null, 'DELETE' ] [ null, '/api/obs/actions/group/wakeup/:pizza' ] (/routes/api/obs-sub-sub.js)
+🚥 2024-04-30T02:49:45.138Z [ null, 'GET' ] [ null, '/api/obs' ] (/routes/api/obs.js)
+🚥 2024-04-30T02:49:45.138Z [ null, 'GET' ] [ null, '/api/obs/:sceneUuid' ] (/routes/api/obs.js)
+🚥 2024-04-30T02:49:45.138Z [ null, 'POST' ] [ null, '/api/obs/resource' ] (/routes/api/obs.js)
+🚥 2024-04-30T02:49:45.138Z [ null, 'GET' ] [ null, '/api/litra' ] (/routes/api/litra.js)
+🚥 2024-04-30T02:49:45.138Z [ null, 'GET' ] [ null, '/api/litra/:id' ] (/routes/api/litra.js)
+🚥 2024-04-30T02:49:45.138Z [ null, 'POST' ] [ null, '/api/litra/dial/:id' ] (/routes/api/litra.js)
+🚥 2024-04-30T02:49:45.138Z [ null, 'POST' ] [ null, '/api/litra/toggle/:id' ] (/routes/api/litra.js)
+🚥 2024-04-30T02:49:45.138Z [ null, 'GET' ] [ null, '/api/screenshot/cleanup' ] (/routes/api/screenshot.js)
+
+## As Middleware Live Logger
+
+```
+app.use(expressRouteLister.log(app, { live: true }));
+```
+
+✅ 2024-04-30T02:49:48.687Z [ 'GET', 'GET' ] [ '/api/obs', '/api/obs' ] (/routes/api/obs.js)
+❌ 2024-04-30T02:50:11.581Z [ 'GET', null ] [ '/api/obsd', null ] ()
+✅ 2024-04-30T02:50:24.593Z [ 'GET', 'GET' ] [ '/api/litra/23424234234', '/api/litra/:id' ] (/routes/api/litra.js)
+✅ 2024-04-30T02:55:00.653Z [ 'GET', 'GET' ] [ '/', '/' ] (/routes/index.js)
+
+## Array of Objects
+
+```
+const routes = expressRouteLister.list(app); // log all routes
+
+console.log(routes);
+```
+
+[
+  {
+    name: 'query',
+    regexp: '/',
+    path: '/',
+    methods: [ '*' ],
+    code: 'function query(req, res, next){\n' +
+    	'    if (!req.query) {\n'...',
+    file: '/routes/index.js'
+  },
+  {
+    name: 'expressInit',
+    regexp: '/',
+    path: '/',
+    methods: [ '*' ],
+    code: 'function expressInit...',
+	file: '/routes/index.js'
+  },
+
+  ...
+
+  {
+    name: 'bound dispatch',
+    regexp: '/api/obs/resource',
+    path: '/api/obs/resource',
+    methods: [ 'POST' ],
+    code: 'async function(req, res)...',
+    file: '/routes/api/obs.js'
+  }
+]
---
+++ index.js
@@ -0,0 +1,152 @@
+/*
+
+	expressRouteLister.log(app)
+
+	console.log(expressRouteLister.list(app));
+
+*/
+
+const _cache = require('module')._cache;
+
+const api = {};
+
+api.parseRegex = (str) =>
+{
+	return !str ? null : str
+		.split('/^')
+		.pop()
+		.replace(/\\/g, '')
+		.split(':^').pop()
+		.split('?(?')[0]
+		.split('(?:([^/]+?))').join(':param') // hold param places
+		.replace('?$/i', '')
+		//.replace(/\(\.\*\)/g, '*') // make wildcard easy to read
+		.toString();
+};
+
+api.formatMethods = (obj) =>
+{
+	return Object.keys(obj || {})
+		.join(',')
+		.toUpperCase()
+		.split(',');
+};
+
+api.locate = (layer) =>
+{
+	for (const id in _cache)
+	{
+		const stack = _cache[id]?.exports?.stack || _cache[id]?.exports._router?.stack;
+		
+		if (stack && stack.indexOf(layer) > -1)
+		{
+			return id.replace(process.env.PWD, '');
+		}
+	}
+
+	return null;
+};
+
+api.list = (app) =>
+{
+	const list = [];
+	const stack = app ? app.stack || app?._router?.stack : null;
+	
+	if (!stack) return list; // no stack, no routes
+
+	const getRoute = (layer, method, pathname) =>
+	{
+		let routes = layer && layer?.regexp.toString().split('|^'); // consider multi route routes
+		
+		if (!layer || !routes || !routes.length) return;
+		
+		for (rt of routes) // multi-path routes
+		{
+			let route = ((pathname || '') + api.parseRegex(rt).substr(pathname ? 1 : 0));
+			let regexp = route.replace(/:param/g, "(?:([^/]+?))");
+
+			// fill params with names
+			if (layer.keys) for (key of layer.keys) route = route.replace(':param', ':' + key.name);
+
+			// if it's middleware
+			if (!layer?.handle?.stack && !layer.route)
+			{
+				list.push({
+					name: layer.name,
+					regexp: regexp.slice(0, -1) || '/',
+					path: (route.slice(0, -1) || '/').replace(/\(\.\*\)/g, '*'),
+					methods: ['*'],
+					code: layer?.handle.toString(),
+					file: api.locate(layer),
+				});
+			}
+
+			// if defined route
+			else if (layer.route)
+			{
+				list.push({
+					name: layer.name,
+					regexp: regexp.slice(0, -1) || '/',
+					path: (route.slice(0, -1) || '/').replace(/\(\.\*\)/g, '*'),
+					methods: api.formatMethods(layer.route.methods),
+					code: layer.route?.stack[0]?.handle.toString(),
+					file: api.locate(layer),
+				});
+			}
+
+			// keep diving through child routes
+			else if (layer?.handle?.stack)
+			{
+				for (l of layer.handle.stack) getRoute(l, null, route);
+			}
+		}
+	};
+
+	// start looking thru paths/layers
+	for (layer of stack) getRoute(layer);
+
+	return list;
+};
+
+api.log = (app = null, options = {}) =>
+{
+	const log = (route, req) =>
+	{
+		let ts = new Date();
+		let methods = [ req ? req.method : null ];
+			methods = methods.concat(route.methods ? (route.methods || null) : null);
+		let paths = [ req ? req.path : null ];
+			paths = paths.concat(route.path ? [ (route.path || null) ] : null);
+		let symbol = '🚥';
+
+		if (req && route.path) symbol = '✅';
+		if (req && !route.path) symbol = '❌';
+
+		console.log(symbol, ts, methods, paths, `(${ route.file || '' })`);
+	};
+
+	// if live logging as middleware
+	if (options && options.live) return function(req, res, next)
+	{
+		const list = api.list(app);
+		
+		const match = list.filter((p) =>
+		{
+			//return p.regexp ? (new RegExp(p.regexp)).test(req._parsedUrl.path) : null;
+			return p.regexp ? (new RegExp('^' + p.regexp + '$')).test(req.path) : null;
+		}).pop();
+
+		log(match || {}, req);
+		
+		next();
+	};
+
+	const list = api.list(app);
+
+	for (let route of list) log(route, null);
+}
+
+module.exports = {
+	list: api.list,
+	log: api.log,
+};
